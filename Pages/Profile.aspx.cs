0d7a9224762e78a8def14e395b606698cf0c6f04
Yehika Bandara <yehikabandara2001@gmail.com>
2026-08-11 11:25:33 +0530

    Profile page updated

--- Pages/Profile.aspx.cs
+++ Pages/Profile.aspx.cs
@@ -1,9 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.UI;
-using System.Web.UI.WebControls;
+using static libraryManagementSystem.Utils.Constants;
 
 namespace libraryManagementSystem.Pages
 {
@@ -11,7 +7,47 @@ namespace libraryManagementSystem.Pages
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            if (Session[SessionKeys.UserId] == null)
+            {
+                Response.Redirect("~/Login.aspx");
+                return;
+            }
 
+            if (!IsPostBack)
+            {
+                LoadProfile();
+            }
+        }
+
+        private void LoadProfile()
+        {
+            string userId = Session[SessionKeys.UserId]?.ToString() ?? "";
+            string username = Session[SessionKeys.Username]?.ToString() ?? "";
+            string fullName = Session[SessionKeys.FullName]?.ToString() ?? "";
+            string role = Session[SessionKeys.Role]?.ToString() ?? "";
+
+            litFullName.Text = fullName;
+            litUsername.Text = username;
+            litRole.Text = role;
+            litInitials.Text = GetInitials(fullName);
+
+            litDetailFullName.Text = fullName;
+            litDetailUsername.Text = username;
+            litDetailRole.Text = role;
+            litDetailUserId.Text = userId;
+        }
+
+        private string GetInitials(string fullName)
+        {
+            if (string.IsNullOrWhiteSpace(fullName))
+                return "?";
+
+            string[] parts = fullName.Trim().Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+            if (parts.Length == 1)
+                return parts[0].Substring(0, 1).ToUpper();
+
+            return (parts[0].Substring(0, 1) + parts[parts.Length - 1].Substring(0, 1)).ToUpper();
         }
     }
 }
